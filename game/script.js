ca51e3b5a73abdee753ed8342e6cda6fcf3ea9bc
dscott414 <67511512+dscott414@users.noreply.github.com>
2021-01-16 05:31:40 -0500

    Update script.js

--- game/script.js
+++ game/script.js
@@ -161,7 +161,7 @@ function loop(ms) {
   
   
   
-  game.timeFoam = game.timeFoam.add(s.times(getTempCompEffect())).div(getTimeFoamEffect())
+  game.timeFoam = game.timeFoam.add((s.times(getTempCompEffect())).div(getTimeFoamEffect()))
   app.$data.tempCompCost = getTempCompCost().beautify(2)
   app.$data.timeFoamEffect = getTimeFoamEffect().beautify(2)
   app.$data.tempCompEffect = getTempCompEffect().beautify(2)
