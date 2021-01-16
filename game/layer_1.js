ff06fb4798ffae7e7fb1733d9df05379f3d7a375
dscott414 <67511512+dscott414@users.noreply.github.com>
2021-01-16 05:59:22 -0500

    Update layer_1.js

--- game/layer_1.js
+++ game/layer_1.js
@@ -85,7 +85,7 @@ function getTempCompEffect() {
 
 function getTimeFoamEffect() {
   if (game.timeFoam.gte(1)) {
-      return game.timeFoam.pow(1.05)
+      return game.timeFoam.pow(1.05.div(game.tempComp))
   } else {
   return 1
   }
