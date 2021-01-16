867ceedcc55ccdf022532e3da507d756277c3897
dscott414 <67511512+dscott414@users.noreply.github.com>
2021-01-16 04:07:12 -0500

    Update layer_1.js

--- game/layer_1.js
+++ game/layer_1.js
@@ -80,7 +80,7 @@ function buyTempComp() {
 }
 
 function getTempCompEffect() {
-  return getTempCompBase().pow(caplog10(game.tempComp).min(15).add(caplog10(getSpacetimeCompEffect())).add(inAnyGalChal()?0:game.superComp.temp)).add(getTimeFoamEffect)
+  return getTempCompBase().pow(caplog10(game.tempComp).min(15).add(caplog10(getSpacetimeCompEffect())).add(inAnyGalChal()?0:game.superComp.temp))
 }
 
 function getTimeFoamEffect() {
