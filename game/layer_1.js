f97291c54476d8ddbb5a1e46663ecb32766c6e9f
dscott414 <67511512+dscott414@users.noreply.github.com>
2021-01-16 03:31:42 -0500

    Update layer_1.js

--- game/layer_1.js
+++ game/layer_1.js
@@ -41,7 +41,6 @@ function getSpaceTimeRate() {
     .times(starMile(16)&&game.spaceless&&inGalChal(1)?1e55:1)
     .times(game.perspectivePower.max(1))
     .times(getSuperNovaEffect(2))
-    .times(.1)
 }
 
 function getSpaceTimeCost() {
